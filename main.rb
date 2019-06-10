9f6e6cbdddb568a93c817d561ce8ec809c68650a
Sahil Jain <sahil@sahil-xps.lan>
2019-06-10 00:39:34 -0500

    Initial commit

---
+++ main.rb
@@ -0,0 +1,19 @@
+def tick args
+  args.outputs.labels << [ 580, 500, 'Hello World!' ]
+  args.outputs.labels << [ 475, 150, '(Consider reading README.txt now.)' ]
+  args.outputs.sprites << [ 576, 310, 128, 101, 'dragonruby.png' ]
+end
+
+def defaults args
+
+
+end
+
+
+def render args
+
+end
+
+def calc args
+
+end
