6871450ec6ab4405b356c692db96adcf43cb6b7d
sahiljain11 <sahilbot1@gmail.com>
2019-06-13 12:47:38 -0500

    Migrated from class structure to entities. Started keyboard input menu navigation

--- main.rb
+++ main.rb
@@ -1,56 +1,44 @@
-
 #Sprites: https://archives.bulbagarden.net/wiki/Category:Yellow_sprites
 #BackSprites: https://pkmn.net/?action=content&page=viewpage&id=8561
 
-class Pokemon
-  name = ""
-  frontSprite = ""
-  backSprite = ""
-
-  currenthp, hp, attack, sattack, defense, sdefense, speed, experience, level = 0
-
-  def initialize (n, fs, bs, stats, exp, l)
-    @name = n
-    @frontSprite = fs
-    @backSprite = bs
-    @currenthp = hp = stats[0];
-    @attack = stats[1]
-    @sattack = stats[2]
-    @defense = stats[3]
-    @sdefense = stats[4]
-    @speed = stats[5]
-    @experience = exp
-    @level = l
-  end
-
-  def getLevel
-    return @level
-  end
-
-  def getFrontSprite
-    return @frontSprite
-  end
-  
-  def getBackSprite
-    return @backSprite
-  end
-  
-end
-
 def defaults args
 
-  args.state.mypokemon ||= Pokemon.new('Bulbasaur', 'sprites/bulbasaur.png', 'sprites/bulbasaurback.png',
-                                       [10, 10, 10, 10, 10, 10], 0, 5);
-
-  args.state.otherpokemon ||= Pokemon.new('Charmander', 'sprites/charmander.png', 'sprites/charmanderback.png',
-                                       [10, 10, 10, 10, 10, 10], 0, 5);
-  args.state.pokmeon1x ||= 200
+  #Base coordinates for the pokemon banners left/right of the pokemon
+  args.state.pokemon1x ||= 200
   args.state.pokemon1y ||= 700
-
   args.state.pokemon2x ||= 750
   args.state.pokemon2y ||= 475
 
-  args.state.pokeballPlacement ||= [[640, 940], [50, 153]]
+  #Pokeball coordinates located in the menu
+  args.state.pokeballPlacementX ||= [840]
+  args.state.pokeballPlacementY ||= [50, 153]
+  args.state.pokeballState ||= 1      #1 is on Fight. 0 is on Item
+
+  #Menu status booleans
+  args.state.choseItem  ||= false
+  args.state.choseFight ||= false
+
+  #Generating entities for my pokemon and opponent pokemon. The map function goes through each iteration of the matrix given.
+  #In the map, basic parameters are defined.
+  args.state.pokemon ||= [
+    [:charmander, 200, 250, 250, 250, true],
+    [:bulbasaur,  825, 475, 300, 300, false]
+  ].map do |name, x, y, width, height, mypokemon|
+    args.state.new_entity(name) do |p|
+      p.name = name.capitalize
+      p.x = x
+      p.y = y
+      p.width = width
+      p.height = height
+      p.level = 5
+      if mypokemon
+        p.sprite = "sprites/" + name.to_s + "back.png"
+      else
+        p.sprite = "sprites/" + name.to_s + ".png"
+      end
+    end
+  end
+
 end
 
 
@@ -61,25 +49,28 @@ def render args
   args.outputs.solids << [0, 0, 1280, 250]
   args.outputs.solids << [15, 15, 1250, 220, 255, 255, 255]
 
-  #Creating the pokemon sprites
-  args.outputs.sprites << [200, 250, 250, 250, args.state.otherpokemon.getBackSprite]
-  args.outputs.sprites << [825, 475, 300, 300, args.state.mypokemon.getFrontSprite]
+  #Creating the pokemon sprites using entities
+  args.outputs.sprites += args.state.pokemon.map {
+    |p|
+    [p.x, p.y, p.width, p.height, p.sprite]
+  }
 
-  #Adding labels
-  args.outputs.labels << [args.state.pokmeon1x, args.state.pokemon1y, 'Bulbasaur Lv. ' + args.state.otherpokemon.getLevel.to_s, 15, 0]
-  args.outputs.labels << [args.state.pokmeon1x + 50, args.state.pokemon1y - 65, 'HP:', 3, 0] 
-  args.outputs.sprites << [210, 545, 300, 100, 'sprites/arrow.png']
+  #Adding opponent pokemon's banner
+  args.outputs.labels << [args.state.pokemon1x, args.state.pokemon1y, args.state.pokemon[1].name.capitalize.to_s + " Lv." +
+                          args.state.pokemon[1].level.to_s, 15, 0]
+  args.outputs.labels << [args.state.pokemon1x + 50, args.state.pokemon1y - 65, 'HP:', 3, 0] 
+  args.outputs.sprites << [args.state.pokemon1x + 10, args.state.pokemon1y - 155, 350, 100, 'sprites/arrow.png']
+  args.outputs.labels << [args.state.pokemon1x + 50, args.state.pokemon1y - 90, 'EXP:', 3, 0]
+  args.outputs.solids << [args.state.pokemon1x + 100, args.state.pokemon1y - 88, 250, 16]
+  args.outputs.solids << [args.state.pokemon1x + 100, args.state.pokemon1y - 113, 250, 16]
+  args.outputs.solids << [args.state.pokemon1x + 102, args.state.pokemon1y - 85, ((1 / 1) * 246).to_i, 10, 0, 255, 0]
+  args.outputs.solids << [args.state.pokemon1x + 102, args.state.pokemon1y - 110, ((1 / 1) * 246).to_i, 10, 0, 191, 255]
 
-  #-----------weird nil bug---------------#
-  #args.outputs.labels << [args.state.pokemon1x + 50, args.state.pokemon1y - 90, 'EXP:', 3, 0]
-  #args.outputs.solids << [args.state.pokmeon1x + 100, args.state.pokemon1y - 88, 250, 16]
-  #args.outputs.solids << [args.state.pokemon1x + 100, args.state.pokemon1y - 113, 250, 16]
-  #args.outputs.solids << [args.state.pokemon1x + 102, args.state.pokemon1y - 85, ((1 / 1) * 246).to_i, 10, 0, 255, 0]
-  #args.outputs.solids << [args.state.pokemon1x + 102, args.state.pokemon1y - 110, ((1 / 1) * 246).to_i, 10, 0, 191, 255]
-
-  
-  args.outputs.labels << [args.state.pokemon2x, args.state.pokemon2y, 'Charmander Lv. ' + args.state.mypokemon.getLevel.to_s, 15, 0]
-  #args.outputs.sprites << [750, 290, 350, 125, 'sprites/flippedArrow.png']
+  #Adding my pokemon's banner
+  args.outputs.labels << [args.state.pokemon2x, args.state.pokemon2y, args.state.pokemon[0].name.capitalize.to_s + " Lv." +
+                          args.state.pokemon[0].level.to_s, 15, 0]
+  args.outputs.sprites << [args.state.pokemon2x, args.state.pokemon2y - 185, 350, 125, 'sprites/flippedArrow.png']
+  #[x, y, text, size, alignment, r, g, b, alpha(transparency), font] Manaspace font
   args.outputs.labels << [args.state.pokemon2x, args.state.pokemon2y - 65, 'HP:', 3, 0]
   args.outputs.labels << [args.state.pokemon2x, args.state.pokemon2y - 90, 'EXP:', 3, 0] 
   args.outputs.solids << [args.state.pokemon2x + 50, args.state.pokemon2y - 88, 250, 16]
@@ -88,16 +79,33 @@ def render args
   args.outputs.solids << [args.state.pokemon2x + 52, args.state.pokemon2y - 85, ((13 / 19) * 246).to_i, 10, 0, 255, 0]
   args.outputs.solids << [args.state.pokemon2x + 52, args.state.pokemon2y - 110, ((1 / 1) * 246).to_i, 10, 0, 191, 255]
 
-  #More work on the box
-  args.outputs.labels << [700, 200, 'FIGHT', 15, 0]
-  args.outputs.labels << [1000, 200, 'PKMN', 15, 0]
-  args.outputs.labels << [700, 100, 'ITEM', 15, 0]
-  args.outputs.labels << [1000, 100, 'RUN', 15, 0]
-  args.outputs.sprites << [args.state.pokeballPlacement[0][1], args.state.pokeballPlacement[1][1], 50, 50, 'sprites/pokeball.png']
+  #Rendering bottom menu
+
+  if !args.state.choseFight && !args.state.choseItem
+    args.outputs.labels << [900, 200, 'FIGHT', 15, 0]
+    args.outputs.labels << [900, 100, 'ITEM', 15, 0]
+    args.outputs.sprites << [args.state.pokeballPlacementX, args.state.pokeballPlacementY[args.state.pokeballState],
+                            50, 50, 'sprites/pokeball.png']
+  end
 
 end
 
 def calc args
+
+  #Keyboard input
+  if args.inputs.keyboard.key_up.w
+    args.state.pokeballState = 1
+  end
+  if args.inputs.keyboard.key_up.s
+    args.state.pokeballState = 0
+  end
+
+  if args.inputs.keyboard.key_up.enter && args.state.pokeballState == 1
+    args.state.choseFight = true
+  end
+  if args.inputs.keyboard.key_up.enter && args.state.pokeballState == 0
+    args.state.choseItem = true
+  end
 end
 
 def tick args
